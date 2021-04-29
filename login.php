d7ab0859d67182029142776e5d398f8764d810b9
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-04-30 03:10:52 +0530

    Only HTML

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>19BIT0292</title>
+</head>
+<body>
+    <form>
+        EMAIL
+        <input type="text" name="em"><br>
+        PASSWORD<input type="password">
+    </form>
+</body>
+</html>
