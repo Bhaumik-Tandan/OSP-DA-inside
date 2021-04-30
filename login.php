a983e912d314915184b5da3dc111431f3f01a2d9
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-04-30 15:02:08 +0530

    PHP and image added

--- login.php
+++ login.php
@@ -3,10 +3,25 @@
     <title>19BIT0292</title>
 </head>
 <body>
-    <form>
+<img src="restricted_acess.jpg" alt="restrcted acess" width="300">
+    <form method="post">
         EMAIL
         <input type="text" name="em"><br>
-        PASSWORD<input type="password">
+        PASSWORD<input type="password" name="pas"><br>
+        <button type="submit">Sign In</button>
     </form>
+    <?php
+         if ($_SERVER["REQUEST_METHOD"] == "POST") 
+         {
+            $email = $_POST['em'];
+            $password = $_POST['pas'];
+            if ($email=="bhaumik.tandan@gmail.com" && $password=="19BIT0292") 
+            {
+                echo "WELCOME";
+            } 
+            else 
+                echo "WRONG CREDENTIALS";
+         }
+    ?>
 </body>
 </html>
